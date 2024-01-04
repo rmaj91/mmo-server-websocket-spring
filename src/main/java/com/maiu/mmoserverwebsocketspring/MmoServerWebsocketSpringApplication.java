2b42dcad30ca29af0af3ce50789c1cb23489712f
rmaj91 <rmaj91@gmail.com>
2024-01-04 22:10:38 +0100

    init

---
+++ src/main/java/com/maiu/mmoserverwebsocketspring/MmoServerWebsocketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.maiu.mmoserverwebsocketspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmoServerWebsocketSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmoServerWebsocketSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maiu/mmoserverwebsocketspring/MmoServerWebsocketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiu.mmoserverwebsocketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmoServerWebsocketSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
